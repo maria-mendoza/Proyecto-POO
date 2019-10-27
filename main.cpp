b6519812072380d3f98f53e426dd00e28145ad2a
maria-mendoza <54276089+maria-mendoza@users.noreply.github.com>
2019-10-27 17:47:23 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,12 @@
+#include "matriz.h
 
+int main{
+  void DibujarMatrizNueva(TipoCaracter **arena );
+  void Mostrar(TipoCaracter **arena);
+  void PosicionarObstaculos(TipoEntero PosicionX, TipoEntero PosicionY );
+}
+
+
+  
+  
+    
