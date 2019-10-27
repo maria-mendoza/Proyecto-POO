8177aaae14dc2bf8f15b02ae7336f652ff16712a
maria-mendoza <54276089+maria-mendoza@users.noreply.github.com>
2019-10-27 15:15:50 -0500

    Update matriz.cpp

--- matriz.cpp
+++ matriz.cpp
@@ -1,23 +1,38 @@
 #include "matriz.h"
 
-TMatriz::TMatriz(): filas{FILAS}, columnas{COLUMNAS} {
-  
-  string matriz[filas][columnas];
+void TMatriz:: MatrizNueva(TipoEntero _filas, TipoEntero _columnas){
+    arena = new TipoCaracter*[_filas];
+    for (size_t i = 0; i < _filas; ++i)
+      arena[i] = new TipoCaracter[_columnas];  
+    }
 
-  for(int i=0; i < filas; i++)
-        {
-                for(int j=0; j < columnas; j++)
-                {
-                        matriz[i][j] = "0";
-                }
-        }
-        matriz[3][2]="🚦";
-        matriz[9][0]="🏁";
+TMatriz::TMatriz(): filas{FILAS}, columnas{COLUMNAS} {
+   MatrizNueva(TipoEntero _filas, TipoEntero _columnas) ;
 }
 
+TMatriz::TMatriz(TipoEntero _filas, TipoEntero _columnas): filas {_filas,}, columnas {_columnas}, CantObstaculos{0} {
+        MatrizNueva(TipoEntero filas, TipoEntero columnas) ;  
+}
 
+TMatriz virtual ~TMatriz() {
+  for (size_t i = 0; i < filas; ++i) {
+    delete[] arena[i];
+  }
+  delete[] arena;
+  arena = nullptr;
+}
+
+void TMatriz:: AdicionarObstaculos(Obstaculo* obstaculos) {
+  Obstaculo** temp = new Obstaculo*[CantObstaculos + 1];
+  for (size_t i  = 0; i < CantObstaculos; ++i)
+    temp[i] = obstaculos[i];
+  temp[obstaculos] = Obstaculo;
+
+  delete [] obstaculos ;
+  obstaculos = temp;
+  obstaculos++;
+}
   
-TMatriz virtual ~TMatriz();
-void AdicionarObstaculos()(Obstaculo* obstaculos);
+
 void ImprimirObstaculos();
 void DibujarMatriz();
