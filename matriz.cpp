4e34f833abfa7a0fd46ef35e5d2657ba85baf922
maria-mendoza <54276089+maria-mendoza@users.noreply.github.com>
2019-10-27 12:49:32 -0500

    Update matriz.cpp

--- matriz.cpp
+++ matriz.cpp
@@ -1,3 +1,15 @@
 #include "matriz.h"
 
-
+void TMatriz::TMatriz(): {
+CBattleBox::CBattleBox(): m_CantRobots{0}, 
+  m_Altura{ALTURA}, m_Ancho{ANCHO}  {
+  /*m_Plano = new TipoCaracter*[m_Altura];
+  for (size_t i = 0; i < m_Altura; ++i)
+    m_Plano[i] = new TipoCaracter[m_Ancho];*/ 
+    resizePlano(m_Altura,m_Ancho) ;
+}
+(TipoEntero altura, TipoEntero ancho);
+TMatriz virtual ~TMatriz();
+void AdicionarObstaculos()(Obstaculo* obstaculos);
+void ImprimirObstaculos();
+void DibujarMatriz();
