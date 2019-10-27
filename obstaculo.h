4511a176c4bb41624e387e9cbdc848cdcd12e38a
maria-mendoza <54276089+maria-mendoza@users.noreply.github.com>
2019-10-27 15:41:17 -0500

    Create obstaculo.h

---
+++ obstaculo.h
@@ -0,0 +1,9 @@
+#include "tipos.h"
+
+class Obstaculo{
+  private: 
+    TipoEntero PosicionX;
+    TipoEntero PosicionY
+  public:
+    PosicionarObstaculos(TipoEntero PosicionX, TipoEntero PosicionY);  
+}
