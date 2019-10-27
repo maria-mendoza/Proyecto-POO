dba5a05e3eca6b79445a08fcafdb1a494727944a
nataliasotelor <54275736+nataliasotelor@users.noreply.github.com>
2019-10-27 18:50:51 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,9 @@
-#include "matriz.h
+#include "matriz.h"
+#include <iostream>
 
 int main{
-  void DibujarMatrizNueva(TipoCaracter **arena );
+  void DibujarMatrizNueva(TipoCaracter **arena);
   void Mostrar(TipoCaracter **arena);
   void PosicionarObstaculos(TipoEntero PosicionX, TipoEntero PosicionY );
 }
-
-
-  
-  
     
