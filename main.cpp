98791a2db3cf9479d57a0d989ea17a01e430c2dd
nataliasotelor <54275736+nataliasotelor@users.noreply.github.com>
2019-11-28 11:12:56 -0500

    main.cpp

--- main.cpp
+++ main.cpp
@@ -7,42 +7,60 @@
 #include "menu.h"
 using  namespace std;
 int main(){
-MenuPrincipalMapas j;
+
 
 int x;
 
   
-    bandera _bandera(0,9);
+    bandera _bandera(9,0);
     semaforo _semaforo(3,2);
     TMatriz campo (10,10);
     mapas map;
-
-    j.ensenarmenu(campo, map);
-    //cin>>siInput;
-    x=j.inputusuario();
-     
-    /* switch (x) {
-        case 1:
-      
-            break;
-        case 2:
-            cout<<"saliste del programa";
-            campo.Liberar (10,10);
-            break;
-        case 3:
-             MenuPrincipalMapas _pmapas ();
-            break;
+     bandera banderaa(9,0);
+    semaforo semaforoo(3,2);
+    TMatriz campoo (10,10);
+    mapas mapaa;
+string route="";
+string routee="";
+   
+int eleccion;
     
- }*/
- 
-    // map.LLenar();
+    
+      cout<<"ssssssss"<<endl;
+      cout<<  "__________Bienvenido al Menú Principal___________ \n";
+      cout   <<  "Este es el mapa aleatorio generado para ti: \n" ;
+      campo.MatrizNueva(10,10);
+      map.LLenar(10,10,campo);
+      campo.Mostrar(10,10);
+               cout   << "Escoge una opción de las siguientes: \n";
+               cout   << "1. Quiero conocer el camino más corto al punto final \n" ;
+               cout   << "2. Quiero salir del programa\n" ;
+                cout   <<"3. Quiero que se genere otro mapa\n";
+                   do{
+    cout<<"Ingrese un número: ";
+    cin>>eleccion;
+      
+  }while(eleccion<1 or eleccion>3);         
+    
+  cout << "Has escogido la opción " <<eleccion<< ".\n";
+switch (eleccion){
 
-          string route=pathFind(_semaforo.getvalorxI(), _semaforo.getvaloryI(), _bandera.getvalorxF(), _bandera.getvaloryF(), campo );
+  case 1: 
+  
+{ int xA=_semaforo.getvalorxI(),
+            yA=_semaforo.getvaloryI(),
+            xB=_bandera.getvalorxF(),
+            yB=_bandera.getvaloryF();
+           route=pathFind(xA, yA,xB ,yB, campo );
+
+           cout<<"route: "<<route<<endl;
                 if(route.length()>0)
                 {
                     int j; char c;
                     int x=_semaforo.getvalorxI();
                     int y=_semaforo.getvaloryI();
+                    cout<<"x: "<<x<<endl;
+                    cout<<"y: "<<y<<endl;
                     campo.getmatriz()[x][y]=2;
                     for(int i=0;i<route.length();i++)
                     {
@@ -52,60 +70,34 @@ int x;
                         y=y+dy[j];
                         campo.getmatriz()[x][y]=3;
                     }
+
                     campo.getmatriz()[x][y]=4;
-
-                    // display the map with the route
-                    for(int y=0;y<m;y++)
-                    {
-                        for(int x=0;x<n;x++)
-                            if(campo.getmatriz()[y][x]==0)
-                                cout<<setw(3)<<"0";
-                            else if(campo.getmatriz()[y][x]==1)
-                                cout<<setw(3)<<"1"; //obstacle
-                            else if(campo.getmatriz()[y][x]==2)
-                                cout<<setw(3)<<"S"; //start
-                            else if(campo.getmatriz()[y][x]==3)
-                                cout<<setw(3)<<"R"; //route
-                            else if(campo.getmatriz()[y][x]==4)
-                                cout<<setw(3)<<"F"; //finish
-                        cout<<endl;
+                  campo.Mostrar(10,10);
+                  cout<<"este es el camino mas corto.";
+        
+              
                     }
+                    if(route=="") 
 
-                    }
-                    if(route=="") cout<<"An empty route generated!"<<endl;
-                    cout<<"este es el camino mas corto.";
+     break; }
 
-
-
-
-
-
-
-
-
-
+case 2:
+  return 0; 
+{cout<<"_________Saliste del Programa_________";
+  break;
+  }
+  case 3:{
+      campoo.MatrizNueva(10,10);
+      mapaa.LLenar(10,10,campoo);
+      campoo.Mostrar(10,10);
+ int xA=_semaforo.getvalorxI(),
+            yA=_semaforo.getvaloryI(),
+            xB=_bandera.getvalorxF(),
+            yB=_bandera.getvaloryF();
     
-    
-
-    //void PosicionarObstaculos(TipoEntero PosicionX, TipoEntero PosicionY );
-    //vector<TMatriz> camino= getShortestPath TMatriz((1,3),TMatriz(4,5),arena,10,10);
-    //for(int i=0;i<camino.size();i++){
-    //cout<<"("<<camino[i].getx()<<", "<<camino[i].gety()<<")"<<endl;
-
-
-
-
-
-    
-/*
-    MenuPrincipalMapas:ensenarmenu();
-    std::cin >> siInput;
-    MenuPrincipalMapas:inputusuario(siInput);
-*/
-
-
-
-return 0;
+      cout<<"¡Se generó otro mapa aleatorio!";
+  }
 
 }
 
+}
