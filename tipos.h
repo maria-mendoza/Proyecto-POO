0b2786b13405eee8b761fd3e64c3ce7c82df0914
nataliasotelor <54275736+nataliasotelor@users.noreply.github.com>
2019-11-28 08:16:20 -0500

    tipos.h

--- tipos.h
+++ tipos.h
@@ -1,10 +1,40 @@
-#ifndef
+//
+// Created by rumin on 25/11/2019.
+//
+
+#ifndef NATRMARR_TIPOS_H
+#define NATRMARR_TIPOS_H
 
 #include <iostream>
+#include <string>
+#include <sstream>
+#include <cstdio>
+#include <iomanip>
+#include <stdlib.h>
+#include <cmath>
+#include <random>
+#include <fstream>
+using namespace std;
+
+
 #include <cstdlib>
 
 using namespace std;
 typedef int TipoEntero;
-typedef char TipoCaracter;
+const int n=10; // horizontal size of the map
+const int m=10; // vertical size size of the map
+static int closed_nodes_map[n][m]; // map of closed (tried-out) nodes
+static int open_nodes_map[n][m]; // map of open (not-yet-tried) nodes
+static int dir_map[n][m]; // map of directions
+const int dir=4; // number of possible directions to go at any position
+// if dir==4
+//static int dx[dir]={1, 0, -1, 0};
+//static int dy[dir]={0, 1, 0, -1};
+// if dir==8
 
-#endif
+static int dx[dir]={1, 0, -1, 0};
+static int dy[dir]={0, 1, 0, -1};
+
+
+
+#endif //NATRMARR_TIPOS_H
