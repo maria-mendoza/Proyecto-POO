7607bd9b14e120b52532340e4298195cc24410b6
nataliasotelor <54275736+nataliasotelor@users.noreply.github.com>
2019-11-28 11:15:57 -0500

    funct.h

--- funct.h
+++ funct.h
@@ -1,7 +1,3 @@
-//
-// Created by rumin on 25/11/2019.
-//
-
 #ifndef NATRMARR_FUNCT_H
 #define NATRMARR_FUNCT_H
 
@@ -9,26 +5,22 @@
 #include "tipos.h"
 #include "algoritmo.h"
 #include "matriz.h"
-// Determine priority (in the priority queue)
 bool operator<(const algoritmo & a, const algoritmo & b)
 {
     return a.getPriority() > b.getPriority();
 }
 
-// A-star algorithm.
-// The route returned is a string of direction digits.
 string pathFind( const int & xStart, const int & yStart,
                  const int & xFinish, const int & yFinish, TMatriz campus  )
 {
-    static priority_queue<algoritmo> pq[2]; // list of open (not-yet-tried) nodes
-    static int pqi; // pq index
+    static priority_queue<algoritmo> pq[2]; 
+    static int pqi; 
     static algoritmo* n0;
     static algoritmo* m0;
     static int i, j, x, y, xdx, ydy;
     static char c;
     pqi=0;
 
-    // reset the node maps
     for(y=0;y<m;y++)
     {
         for(x=0;x<n;x++)
@@ -37,34 +29,27 @@ string pathFind( const int & xStart, const int & yStart,
             open_nodes_map[x][y]=0;
         }
     }
-
-    // create the start node and push into list of open nodes
+cout<<"Map Size (X,Y): "<<"10"<<","<<"10"<<endl;
+    cout<<"Start: "<<xStart<<","<<yStart<<endl;
+    cout<<"Finish: "<<xFinish<<","<<yFinish<<endl;
     n0=new algoritmo(xStart, yStart, 0, 0);
     n0->updatePriority(xFinish, yFinish);
     pq[pqi].push(*n0);
-    open_nodes_map[x][y]=n0->getPriority(); // mark it on the open nodes map
-
-    // A* search
+    open_nodes_map[x][y]=n0->getPriority(); 
     while(!pq[pqi].empty())
     {
-        // get the current node w/ the highest priority
-        // from the list of open nodes
         n0=new algoritmo( pq[pqi].top().getxPos(), pq[pqi].top().getyPos(),
                      pq[pqi].top().getLevel(), pq[pqi].top().getPriority());
 
         x=n0->getxPos(); y=n0->getyPos();
 
-        pq[pqi].pop(); // remove the node from the open list
+        pq[pqi].pop();
         open_nodes_map[x][y]=0;
-        // mark it on the closed nodes map
         closed_nodes_map[x][y]=1;
 
-        // quit searching when the goal state is reached
-        //if((*n0).estimate(xFinish, yFinish) == 0)
+
         if(x==xFinish && y==yFinish)
         {
-            // generate the path from finish to start
-            // by following the directions
             string path="";
             while(!(x==xStart && y==yStart))
             {
@@ -75,14 +60,12 @@ string pathFind( const int & xStart, const int & yStart,
                 y+=dy[j];
             }
 
-            // garbage collection
+      
             delete n0;
-            // empty the leftover nodes
             while(!pq[pqi].empty()) pq[pqi].pop();
             return path;
         }
 
-        // generate moves (child nodes) in all possible directions
         for(i=0;i<dir;i++)
         {
             xdx=x+dx[i]; ydy=y+dy[i];
@@ -90,40 +73,29 @@ string pathFind( const int & xStart, const int & yStart,
             if(!(xdx<0 || xdx>n-1 || ydy<0 || ydy>m-1 || campus.getplano()[xdx][ydy]==1
                  || closed_nodes_map[xdx][ydy]==1))
             {
-                // generate a child node
                 m0=new algoritmo( xdx, ydy, n0->getLevel(),
                              n0->getPriority());
                 m0->nextLevel(i);
                 m0->updatePriority(xFinish, yFinish);
-
-                // if it is not in the open list then add into that
                 if(open_nodes_map[xdx][ydy]==0)
                 {
                     open_nodes_map[xdx][ydy]=m0->getPriority();
                     pq[pqi].push(*m0);
-                    // mark its parent node direction
                     dir_map[xdx][ydy]=(i+dir/2)%dir;
                 }
                 else if(open_nodes_map[xdx][ydy]>m0->getPriority())
                 {
-                    // update the priority info
                     open_nodes_map[xdx][ydy]=m0->getPriority();
-                    // update the parent direction info
+                  
                     dir_map[xdx][ydy]=(i+dir/2)%dir;
-
-                    // replace the node
-                    // by emptying one pq to the other one
-                    // except the node to be replaced will be ignored
-                    // and the new node will be pushed in instead
                     while(!(pq[pqi].top().getxPos()==xdx &&
                             pq[pqi].top().getyPos()==ydy))
                     {
                         pq[1-pqi].push(pq[pqi].top());
                         pq[pqi].pop();
                     }
-                    pq[pqi].pop(); // remove the wanted node
+                    pq[pqi].pop();
 
-                    // empty the larger size pq to the smaller one
                     if(pq[pqi].size()>pq[1-pqi].size()) pqi=1-pqi;
                     while(!pq[pqi].empty())
                     {
@@ -131,14 +103,14 @@ string pathFind( const int & xStart, const int & yStart,
                         pq[pqi].pop();
                     }
                     pqi=1-pqi;
-                    pq[pqi].push(*m0); // add the better node instead
+                    pq[pqi].push(*m0); 
                 }
-                else delete m0; // garbage collection
+                else delete m0; 
             }
         }
-        delete n0; // garbage collection
+        delete n0; 
     }
-    return ""; // no route found
+    return ""; 
 }
 
 
